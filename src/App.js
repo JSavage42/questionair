f59b3336fb25e0dd935a283454ae751410b79abc
Jason Savage <32180237+JSavage42@users.noreply.github.com>
2018-11-06 16:56:38 -0600

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Question Air</h1>
+        </header>
+        <h2>Test H2</h2>
+        <a href="#" title="Test Link">Test Link</a>
+      </div>
+    );
+  }
+}
+
+export default App;
