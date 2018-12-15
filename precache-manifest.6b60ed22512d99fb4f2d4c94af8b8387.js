0b731cbeb8ac90285133e1ea9beb9b7912c8690a
Jason Savage <jasavage42@gmail.com>
2018-12-15 14:46:23 -0600

    Updates

---
+++ precache-manifest.6b60ed22512d99fb4f2d4c94af8b8387.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0be5f3d163ca4fabeda4",
+    "url": "/questionair/static/css/main.587aca6a.chunk.css"
+  },
+  {
+    "revision": "0be5f3d163ca4fabeda4",
+    "url": "/questionair/static/js/main.0be5f3d1.chunk.js"
+  },
+  {
+    "revision": "4d64685c96c2450a2533",
+    "url": "/questionair/static/js/1.4d64685c.chunk.js"
+  },
+  {
+    "revision": "66417dad9f5a9c329fce",
+    "url": "/questionair/static/js/runtime~main.66417dad.js"
+  },
+  {
+    "revision": "6efe43468396631090a82e3252fa881d",
+    "url": "/questionair/index.html"
+  }
+];
