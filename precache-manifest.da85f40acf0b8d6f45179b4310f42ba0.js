76147f998da1d93e77100ab91d66ca846129ea2d
Jason Savage <jasavage42@gmail.com>
2018-12-26 10:14:50 -0600

    Updates

---
+++ precache-manifest.da85f40acf0b8d6f45179b4310f42ba0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "74b8ed0b474e72c86dfb",
+    "url": "/questionair/static/css/main.afb10d06.chunk.css"
+  },
+  {
+    "revision": "74b8ed0b474e72c86dfb",
+    "url": "/questionair/static/js/main.74b8ed0b.chunk.js"
+  },
+  {
+    "revision": "d78daf7214a8bfcc277c",
+    "url": "/questionair/static/js/1.d78daf72.chunk.js"
+  },
+  {
+    "revision": "66417dad9f5a9c329fce",
+    "url": "/questionair/static/js/runtime~main.66417dad.js"
+  },
+  {
+    "revision": "11ad7f58c5604769ec4064da39c77392",
+    "url": "/questionair/static/media/logo.11ad7f58.svg"
+  },
+  {
+    "revision": "635974b79bcd235e636b307eb1439a12",
+    "url": "/questionair/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/questionair/precache-manifest.37d074f4b5ca487f2ba9879306a91529.js"
+  "/questionair/precache-manifest.da85f40acf0b8d6f45179b4310f42ba0.js"
 );
 
 workbox.clientsClaim();
